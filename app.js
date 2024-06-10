ae3009709fef6789876f04202f5ecde30b3c0fa9
Ronggang Cui <ronggangcui@Ronggangs-Mac-Studio.local>
2024-06-09 22:28:36 -0600

    seeding data added to mongo, starting ejs files created

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+    console.log('Connected to the database');
+}
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({title: 'My Backyard', description: 'Cheap camping!'});
+    await camp.save();
+    res.send(camp);
+});
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
 });
 
 app.listen(3000, () => {
---
+++ seeds/index.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+    console.log('Connected to the database');
+}
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)}, ${sample(places)}`,
+            description: 'Cheap camping!',
+            price: Math.floor(Math.random() * 20) + 10
+        });
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
