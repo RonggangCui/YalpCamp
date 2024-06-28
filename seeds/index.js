b3b0fc5a337ef53e9d86ee8fb52ae54dcdf12ca9
Ronggang Cui <rongcuidev@gmail.com>
2024-06-28 00:09:49 -0600

    refined the UI for all the pages

--- seeds/index.js
+++ seeds/index.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
-const Campground = require('../models/campground');
-const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
-    console.log('Connected to the database');
+    await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+    console.log("Connected to the database");
 }
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
@@ -19,12 +19,14 @@ const seedDB = async () => {
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)}, ${sample(places)}`,
-            description: 'Cheap camping!',
-            price: Math.floor(Math.random() * 20) + 10
+            image: "https://picsum.photos/400?random=${Math.random()}",
+            description:
+                "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi iusto sunt vel laborum magnam dolor aperiam, impedit numquam hic pariatur ut incidunt perferendis ducimus sed qui ab quos, fugiat iste.",
+            price: Math.floor(Math.random() * 20) + 10,
         });
         await camp.save();
     }
-}
+};
 
 seedDB().then(() => {
     mongoose.connection.close();
